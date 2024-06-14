8e98cdbe89cd2b318e73183a1e0953c09105873f
Aydar Kutlubaev <Aydark30@gmail.com>
2024-06-15 01:45:27 +0500

    save

---
+++ src/main/java/web/controller/CarController.java
@@ -0,0 +1,48 @@
+package web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class CarController {
+
+    public List<Car> createCars() {
+        List<Car> cars = new ArrayList<Car>();
+        cars.add(new Car("BMW","m3","Black"));
+        cars.add(new Car("LADA","Priora","White"));
+        cars.add(new Car("Opel","Astra","Pink"));
+        cars.add(new Car("KAMAZ","Samosval","Orange"));
+        cars.add(new Car("KIA","Rio","Yellow"));
+        return cars;
+    }
+
+    @GetMapping(value="/cars")
+    public String printCars(@RequestParam(value="count") int count, Model model) {
+//        List<Car> cars = new ArrayList<Car>();
+//        cars.add(new Car("BMW","m3","Black"));
+//        cars.add(new Car("LADA","Priora","White"));
+//        cars.add(new Car("Opel","Astra","Pink"));
+//        cars.add(new Car("KAMAZ","Samosval","Orange"));
+//        cars.add(new Car("KIA","Rio","Yellow"));
+        model.addAttribute("cars", outCars(count,createCars()));
+        return "cars";
+    }
+
+    public List<Car> outCars(int count, List<Car> cars) {
+        if (cars.size() < count) {
+            return cars;
+        }
+        List<Car> carNew = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            carNew.add(cars.get(i));
+        }
+        return carNew;
+    }
+}
--- src/main/java/web/model/Car.java
+++ src/main/java/web/model/Car.java
@@ -1,5 +1,49 @@
 package web.model;
 
-public class Car {
+import org.springframework.stereotype.Component;
 
+@Component
+public class Car {
+    String model;
+    String series;
+    String color;
+    public Car(String model, String series, String color) {
+        this.model = model;
+        this.series = series;
+        this.color = color;
+    }
+    public Car(){}
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getSeries() {
+        return series;
+    }
+
+    public void setSeries(String series) {
+        this.series = series;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", series='" + series + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
 }
